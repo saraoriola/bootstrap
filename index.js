94a55e6e7a5323be89d9cb05e594e482d8891b35
saraoriola <saraoriola@outlook.com>
2023-06-09 09:30:16 +0200

    aleluya

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const button = document.querySelector('#submit');
 const fail = document.querySelector('.error');
 const successAlert = document.querySelector('.alert-success');
 const dangerAlert = document.querySelector('.alert-danger');
-const informacion = localStorage.setItem(getName.value, getEmail.value);
-const pintar = document.querySelector('.card-text');
+const informacion = JSON.parse(localStorage.getItem("users"));
+const pintar = document.querySelector('#cardUser');
 
+const users=informacion ||[]
 //COMPROBAMOS QUE LA INFORMACION SEA CORRECTA
 const verify = (e) => {
     e.preventDefault();
     const password = /^[a-zA-Z]\w{3,14}$/;
     const email = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-    
+    const user={
+        name:getName.value,
+        email:getEmail.value,
+        pass:pass.value
+    }
     if (getName.value === '' || getEmail.value === '' || pass.value === '' || pass2.value === '') {
         fail.innerHTML = 'Rellena todos los campos';
         dangerAlert.classList.remove('hide');
@@ -28,9 +33,13 @@ const verify = (e) => {
         fail.innerHTML = 'Las contraseñas deben ser iguales';
         dangerAlert.classList.remove('hide');
     } else {
-        successAlert.classList.remove('hide');
+        users.push(user)
+        localStorage.setItem("users",JSON.stringify(users))
+        paintCard()
+        successAlert.classList.remove('d-none');
         setTimeout(() => {
             window.location.href = "user.html";
+            putInCard()
         }, 3000);
     }
     
@@ -41,9 +50,31 @@ const verify = (e) => {
 };
 
 //LE DAMOS LA FUNCION AL BOTON SUBMIT
-button.addEventListener('click', verify);
+button?.addEventListener('click', verify);
 
-const dibujar = () => {
-    pintar.innerHTML = `Nombre: ${getName.value} Email: ${getEmail.value}`;
-};
 
+
+const paintCard =() => {
+    const users = JSON.parse(localStorage.getItem("users"))
+    if(users && pintar){
+        for (const user of users) {
+                console.log(pintar)
+                
+                pintar.innerHTML += `
+                <div class="card-overlay">
+          <div class="card text-center">
+            <div class="card-header">
+            ${user.name}
+            </div>
+            <div class="card-body">
+              <h5 class="card-title" ></h5>
+              <p class="card-text" id="cardEmail"> ${user.email}</p>
+              <a href="/menu/catalogo.html" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>`
+            }
+    }
+    
+}
+paintCard()
